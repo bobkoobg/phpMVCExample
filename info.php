f3bf7c5014192fb05beeaad014d7485688d380cb
bobkoobg <boyko.surlev@gmail.com>
2016-10-07 12:47:21 +0200

    Initial commit - php basic setup and apache2/htacess setup + git ignore on sublime projects

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+	echo "I AZ OBICHAM MUSAKA!!!";
+	echo "Ti potursi : ", $_GET['url'];
+
+	$page = $_GET['url'];
+
+	if ( !empty($page) ) {
+		echo "<p>ze page is not emptiii</p>";
+	} else {
+		echo "<p>you want main page true?</p>";
+	}
+
+
