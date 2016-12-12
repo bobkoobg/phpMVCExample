72442eeced03b739fa7927025119aa2d302da0d9
bobkoobg <boyko.surlev@gmail.com>
2016-12-12 21:27:44 +0100

    Fix: Popping elements on front page if many mouseover events are triggered at once. Fix: Styling of search bar

--- view/js/index.js
+++ view/js/index.js
@@ -1,3 +1,5 @@
+var timeoutId;
+
 setTimeout( function() {
 	function init() {
 	 	console.log("Index.js loaded...");
@@ -5,17 +7,35 @@ setTimeout( function() {
 	 	$(".sub-company-wrapper").mouseenter(
 	 		function(){
 	 			var currWrapper = $(this);
-		    	console.log("ON HOVER IN", $(this));
-	 			currWrapper.find(".main-container").fadeOut();
-	 			currWrapper.find(".secondary-container").fadeIn();
+	 			if (!timeoutId) {
+			        timeoutId = window.setTimeout(function() {
+			            timeoutId = null; // EDIT: added this line
+			 			currWrapper.find(".main-container").fadeOut(function() {
+			 				currWrapper.find(".secondary-container").fadeIn( function () {
+			 					currWrapper.find(".main-container").hide();
+			 					currWrapper.find(".secondary-container").show();
+			 				});
+			 				
+			 			});
+			       }, 1000);
+			    }
 		    });
 
 	 	$(".sub-company-wrapper").mouseleave(
 	 		function(){
-		    	var currWrapper = $(this);
-		    	console.log("ON HOVER OUT", $(this));
-		    	currWrapper.find(".main-container").fadeIn();
-	 			currWrapper.find(".secondary-container").fadeOut();
+		        var currWrapper = $(this);
+	 			if (timeoutId) {
+			        window.clearTimeout(timeoutId);
+			        timeoutId = null;
+			    }
+			    else {
+			    	currWrapper.find(".secondary-container").fadeOut(function() {
+			    		currWrapper.find(".main-container").fadeIn(function() {
+			    			currWrapper.find(".main-container").show();
+		 					currWrapper.find(".secondary-container").hide();
+			    		});
+			    	});
+			    }
 		    });
 	}
 
